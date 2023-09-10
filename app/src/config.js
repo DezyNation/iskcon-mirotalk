a47233a48d492f6681bcdb6ba612e61cc2a4b7b3
DezyNation <87603422+DezyNation@users.noreply.github.com>
2023-09-10 20:15:47 +0530

    audio bitrate

--- app/src/config.js
+++ app/src/config.js
@@ -143,7 +143,7 @@ module.exports = {
                 {
                     kind: 'audio',
                     mimeType: 'audio/opus',
-                    clockRate: 24000,
+                    clockRate: 12800,
                     channels: 2,
                 },
                 {
