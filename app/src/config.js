df74d0f0f72a459fdb21b7291e6b19ab405885f8
DezyNation <87603422+DezyNation@users.noreply.github.com>
2023-09-11 17:35:04 +0530

    port inc to 40400

--- app/src/config.js
+++ app/src/config.js
@@ -133,7 +133,7 @@ module.exports = {
         numWorkers: Object.keys(os.cpus()).length,
         worker: {
             rtcMinPort: 40000,
-            rtcMaxPort: 40300,
+            rtcMaxPort: 40400,
             logLevel: 'error',
             logTags: ['info', 'ice', 'dtls', 'rtp', 'srtp', 'rtcp', 'rtx', 'bwe', 'score', 'simulcast', 'svc', 'sctp'],
         },
